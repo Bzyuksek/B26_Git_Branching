0debafce2dbebfc9629edaf44578f367834e5840
Bzyuksek <117347949+Bzyuksek@users.noreply.github.com>
2023-02-06 00:38:52 +0300

    Update Tc001.java

--- src/test_cases/Tc001.java
+++ src/test_cases/Tc001.java
@@ -5,9 +5,7 @@ public class Tc001 {
     public static void main(String[] args) {
 // testing
 
-        //more changes
-//more changes
-        //more changes
+        
         //more changes
         //more changes
         //more changes
